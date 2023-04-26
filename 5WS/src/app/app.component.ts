f168088b3963ad996da27dcf77c42a8eeab00ce3
gabi <gabika.hun@gmail.com>
2023-04-26 20:30:34 +0200

    Done :)

--- 5WS/src/app/app.component.ts
+++ 5WS/src/app/app.component.ts
@@ -10,15 +10,106 @@ export class AppComponent {
   title = 'WS5';
 
   products: Array<Product> = [];
+  
+  currentProduct : Product = new Product();
 
   constructor() {
     this.fetchData();
   }
 
+  counter(){
+    return this.products.length;
+  }
+
+  rowColor(stock: number): string {
+    if (stock > 0 && stock <= 50) {
+      return 'table-danger'; // alacsony készlet --> piros
+    } else if (stock > 50 && stock <= 100) {
+      return 'table-warning'; // közepes készlet --> sárga
+    } else if (stock > 100) {
+      return 'table-success'; // nagy készlet --> zöld
+    } else {
+      return ''; // default value, no specific class
+    }
+  }
+  deleteProduct(id: number) {
+    fetch(`https://dummyjson.com/products/${id}`, {
+      method: 'DELETE',
+    })
+      .then((res) => res.json())
+      .then(console.log);
+
+      this.products = this.products.filter(x=>x.id!== id);
+  }
+
+  moreButtonSelect(product : Product) {
+    this.currentProduct = product;
+    console.log(this.currentProduct)
+  }
+
+  updateProduct(): void {
+    fetch(`https://dummyjson.com/products/${this.currentProduct.id}`, {
+      method: 'PUT',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(this.currentProduct)
+    })
+    .then(res => res.json())
+    .then(updatedProduct => {
+      console.log('Updated product:', updatedProduct);
+
+      const index = this.products.findIndex(
+        product => product.id === updatedProduct.id
+      );
+
+      if (index !== -1) {
+        this.products[index] = updatedProduct;
+      }
+    });
+  }
+
+  countBrands() {
+    const brandCounts: {[brand: string]: number} = {};
+  
+    this.products.forEach(product => {
+      if (brandCounts[product.brand]) {
+        brandCounts[product.brand]++;
+      } else {
+        brandCounts[product.brand] = 1;
+      }
+    });
+  
+    let result = 'Brand Counts:\n';
+    for (const brand in brandCounts) {
+      if (brandCounts.hasOwnProperty(brand)) {
+        result += `${brand}: ${brandCounts[brand]}\n`;
+      }
+    }
+    alert(result);
+  }
+  
+
+  avgRatings() {
+    const sumRatings = this.products.reduce((sum, product) => sum + product.rating, 0);
+    const averageRating = sumRatings / this.products.length;
+  
+    alert(`Average rating: ${averageRating}`);
+  }
+  
+  stockLevel() {
+    const avgStock = this.products.reduce((sum, product) => sum + product.stock, 0) / this.products.length;
+    const belowAvgStock = this.products.filter(product => product.stock < avgStock)
+      .map(product => `ID: ${product.id}, Title: ${product.title}`);
+  
+    alert(`Products below average stock level:\n${belowAvgStock.join('\n')}`);
+  }
+  
+  
+
+
   async fetchData() {
     fetch('https://dummyjson.com/products')
-      .then(res => res.json())
-      .then(objects => {
+      .then((res) => res.json())
+      .then((objects) => {
         objects.products.map((x: any) => {
           let t = new Product();
           t.id = x.id;
@@ -32,7 +123,7 @@ export class AppComponent {
           t.category = x.category;
           t.thumbnail = x.thumbnail;
           t.images = x.images;
-  
+
           this.products.push(t);
         });
         console.log(this.products);
